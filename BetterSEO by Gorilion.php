6f2b2f0771f6e478f3e20499450c0e22eeb26177
GermanWainfeld <98604014+GermanWainfeld@users.noreply.github.com>
2025-10-08 12:12:29 -0300

    Update BetterSEO by Gorilion.php

--- BetterSEO by Gorilion.php
+++ BetterSEO by Gorilion.php
@@ -3,7 +3,7 @@
  * Plugin Name: BetterSEO by Gorilion
  * Plugin URI: https://www.gorilion.com/better-seo/
  * Description: Dynamically enable code for Rank Math or Yoast SEO, and update from GitHub.
- * Version:     1.31
+ * Version:     1.32
  * Author:      Gorilion
  * Author URI:  https://www.gorilion.com
  * License:     GPL2
@@ -26,7 +26,7 @@ if (!defined('ABSPATH')) {
 
 // Define the plugin version constant (used in debugging comments)
 if (!defined('BETTERSEO_VERSION')) {
-    define('BETTERSEO_VERSION', '1.31');
+    define('BETTERSEO_VERSION', '1.32');
 }
 
 /**
