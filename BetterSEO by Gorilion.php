67097368d4f31cf69ed743c334d5067ce08a39cf
Mariano de Iriondo <miriondo@gmail.com>
2025-11-17 14:24:09 -0300

    Improve to get full URL

--- BetterSEO by Gorilion.php
+++ BetterSEO by Gorilion.php
@@ -330,7 +330,7 @@ function gorilion_seo_switcher_inject_functions()
 			}
 
 			// Get the request URL to determine slug.
-			$request_url = $_SERVER['REQUEST_URI'];
+			$request_url = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
 			$request_url = trim($request_url, '/');
 			$parts = explode('/', $request_url);
 			$result = end($parts);
@@ -506,7 +506,7 @@ function gorilion_seo_switcher_inject_functions()
 			}
 
 			// Similar logic to get $result from the request URI.
-			$request_url = $_SERVER['REQUEST_URI'];
+			$request_url = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
 			$request_url = trim($request_url, '/');
 			$parts = explode('/', $request_url);
 			$result = end($parts);
